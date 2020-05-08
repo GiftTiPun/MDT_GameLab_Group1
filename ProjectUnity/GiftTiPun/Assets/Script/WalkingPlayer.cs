993e9066b094cf541f50505504ad6e0eaeebb7e5
Marzanos <punthita.marz@mail.kmutt.ac.th>
2020-05-08 13:19:26 +0700

    Update_WalkandRotate

--- ProjectUnity/GiftTiPun/Assets/Script/WalkingPlayer.cs
+++ ProjectUnity/GiftTiPun/Assets/Script/WalkingPlayer.cs
@@ -5,10 +5,7 @@ using UnityEngine;
 public class WalkingPlayer : MonoBehaviour
 {
     public float SpeedMovement = 3.0f;
-    public float speedH = 2.0f;
-    public float speedV = 2.0f;
-    private float X = 0.0f;
-    private float Y = 0.0f;
+    
 
 
     void Start()
