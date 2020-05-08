7d7b875fc9fb7b8b60608ded57fbc508241f34f3
Marzanos <punthita.marz@mail.kmutt.ac.th>
2020-05-08 17:15:38 +0700

    Script_Attack

---
+++ ProjectUnity/GiftTiPun/Assets/Script/PlayerAttack.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    public float attack_Power = 10f;
+    public float attack_Range = 15f;
+    public float TimebetweenAttack = 0.5f;
+    public GameObject target;
+    private Vector3 targetposition;
+    
+    
+
+    private void Attack()
+    {
+        Debug.Log("Attack");
+    }
+
+    private void Start()
+    {
+        target = GameObject.FindWithTag("enemy");
+        targetposition = target.transform.position;
+    }
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Attack();
+        }
+    }
+    
+}
