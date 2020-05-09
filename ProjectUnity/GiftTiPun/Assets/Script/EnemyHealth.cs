fc595e5ad5952d7899627eda5115b343a391dc58
Marzanos <punthita.marz@mail.kmutt.ac.th>
2020-05-09 13:51:39 +0700

    Add_AttackScript

---
+++ ProjectUnity/GiftTiPun/Assets/Script/EnemyHealth.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    public float enemy_max_health = 50;
+    public float enemy_current_health = 50;
+    public GameObject body;
+
+    public void adjustcurrenthealth(float adj)
+    {
+        enemy_current_health += adj;
+
+        if (enemy_current_health <= 0)
+        {
+            enemy_current_health = 0;
+            Destroy(body);
+        }
+        if (enemy_current_health > enemy_max_health)
+        {
+            enemy_current_health = enemy_max_health;
+        }
+        if (enemy_max_health < 1)
+        {
+            enemy_max_health = 1;
+        }
+    }
+    
+    void Update()
+    {
+        adjustcurrenthealth(0);
+    }
+}
--- ProjectUnity/GiftTiPun/Assets/Script/PlayerAttack.cs
+++ ProjectUnity/GiftTiPun/Assets/Script/PlayerAttack.cs
@@ -4,23 +4,33 @@ using UnityEngine;
 
 public class PlayerAttack : MonoBehaviour
 {
-    public float attack_Power = 10f;
-    public float attack_Range = 15f;
+    public float attack_Power = -10f;
     public float TimebetweenAttack = 0.5f;
-    public GameObject target;
-    private Vector3 targetposition;
+    GameObject target;
     
     
 
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "enemy")
+        {
+            EnemyHealth Ehealth = other.gameObject.GetComponent<EnemyHealth>();
+            Ehealth.adjustcurrenthealth(attack_Power);
+            
+        }
+        
+    }
     private void Attack()
     {
         Debug.Log("Attack");
+        
     }
 
     private void Start()
     {
         target = GameObject.FindWithTag("enemy");
-        targetposition = target.transform.position;
+        
     }
     void Update()
     {
