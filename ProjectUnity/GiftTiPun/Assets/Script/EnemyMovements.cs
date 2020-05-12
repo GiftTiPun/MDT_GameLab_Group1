fdfffa82dfcf3d16a157a0177aa7810084763f96
YomiDaze <chanitra.giftchr@mail.kmutt.ac.th>
2020-05-12 19:24:23 +0700

    enemyfollow,walking,attack

---
+++ ProjectUnity/GiftTiPun/Assets/Script/EnemyMovements.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovements : MonoBehaviour
+{
+    Transform player;               // Reference to the player's position.
+    PlayerHealth1 playerHealth;      // Reference to the player's health.
+    EnemyHealth1 enemyHealth;        // Reference to this enemy's health.
+    UnityEngine.AI.NavMeshAgent nav;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    void Awake()
+    {
+        // Set up the references.
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        playerHealth = player.GetComponent<PlayerHealth1>();
+        enemyHealth = GetComponent<EnemyHealth1>();
+        nav = GetComponent<UnityEngine.AI.NavMeshAgent>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        // If the enemy and the player have health left...
+        if (enemyHealth.currentHealth > 0 && playerHealth.currentHealth > 0)
+        {
+            // ... set the destination of the nav mesh agent to the player.
+            nav.SetDestination(player.position);
+        }
+        // Otherwise...
+        else
+        {
+            // ... disable the nav mesh agent.
+            nav.enabled = false;
+        }
+    }
+}
---
+++ ProjectUnity/GiftTiPun/Assets/Script/LocalPlayerSetup.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LocalPlayerSetup : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject.FindGameObjectWithTag("EnemyManager").SetActiveRecursively(true);
+
+        if (isLocalPlayer)
+        {
+            GameObject.FindGameObjectWithTag("MainCamera").GetComponent<camerarotation1>().enabled = true;
+            GetComponent<WalkingPlayer1>().enabled = true;
+            GetComponentInChildren<PlayerAttack>().enabled = true;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
