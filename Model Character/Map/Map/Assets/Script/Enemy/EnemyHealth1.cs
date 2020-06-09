e4b1555065c549cd38d5e67bcae926d8096a5037
Marzanos <punthita.marz@mail.kmutt.ac.th>
2020-06-09 22:48:27 +0700

    Add_Code_Walking_in_map
    - test walking code
    -fix bug in potion script
    - fix bug in waling code when in the map
    - check ontrigger of the pond to enter area

---
+++ Model Character/Map/Map/Assets/Script/Enemy/EnemyHealth1.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
+
+public class EnemyHealth1 : MonoBehaviour
+{
+    public float enemy_max_health = 50;
+    public float enemy_current_health = 50;
+    public GameObject body;
+    
+
+    public void adjustcurrenthealth(float adj)
+    {
+        enemy_current_health += adj;
+        Score addscore = GameObject.Find("player").GetComponent<Score>(); //to use find gameobject that contain score of each level.
+
+        if (enemy_current_health <= 0)
+        {
+            enemy_current_health = 0;
+            Destroy(body);
+            addscore.score++;
+
+        }
+        if (enemy_current_health > enemy_max_health)
+        {
+            enemy_current_health = enemy_max_health;
+        }
+        if (enemy_max_health < 1)
+        {
+            enemy_max_health = 1;
+        }
+    }
+
+    void Update()
+    {
+        adjustcurrenthealth(0);
+    }
+}
