df9bd054b85f3c13cf74f14843d7de7760360ddd
YomiDaze <chanitra.giftchr@mail.kmutt.ac.th>
2020-06-20 22:00:56 +0700

    Add_Prologue
    เหลือเชื่อง หลอดโหลด คัทซีน

---
+++ Model Character/Map/Map/Assets/Script/SceneManage/Movie.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+public class Movie : MonoBehaviour
+{
+    public RawImage rawImage;
+    public VideoPlayer videoPlayer;
+    public AudioSource audioSource;
+
+    void Start()
+    {
+        StartCoroutine(PlayVideo());
+
+    }
+
+    IEnumerator PlayVideo()
+    {
+        WaitForSeconds waitForSeconds = new WaitForSeconds(1);
+        while (!videoPlayer.isPrepared)
+        {
+            yield return waitForSeconds;
+            break;
+        }
+        rawImage.texture = videoPlayer.texture;
+        videoPlayer.Play();
+        audioSource.Play();
+    }
+}
---
+++ Model Character/Map/Map/Assets/Script/SceneManage/loadscene.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class loadscene : MonoBehaviour
+{
+    float countdown = 0f;
+    public Slider loadbar;
+    AsyncOperation operation;
+    float progress = 0f;
+
+    private void Start()
+    {
+        loadbar = GetComponent<Slider>();
+        StartCoroutine(Load(1));
+    }
+    IEnumerator Load (int scene)
+    {
+        operation = SceneManager.LoadSceneAsync(scene);
+        operation.allowSceneActivation = false;
+
+        while( !operation.isDone)
+        {
+            progress = Mathf.Clamp01(operation.progress / .9f);
+            Debug.Log(progress);
+            
+
+            if ( operation.progress >= 0.9f )
+            {
+                Invoke("nextscene", 5f); 
+            }
+            yield return null;
+        }
+    }
+
+    private void nextscene()
+    {
+        operation.allowSceneActivation = true;
+
+    }
+    private void Update()
+    {
+        loadbar.value = progress;
+    }
+}
