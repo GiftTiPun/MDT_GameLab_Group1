38086653131c8ff978b0e813cbb42056cdcafc64
Marzanos <punthita.marz@mail.kmutt.ac.th>
2020-06-20 17:02:45 +0700

    Fix_Bug
    - ใส่เก็บเจมแล้วได้อะไร
    - แก้ bug กลับหัว
    - ใส่หน้า pause
    - แก้ บัคกระโดด

---
+++ Model Character/Map/Map/Assets/Script/item/GemcanvasDestroyer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.PlayerLoop;
+
+public class GemcanvasDestroyer : MonoBehaviour
+{
+    public GameObject GemCanvas;
+
+
+    private void Update()
+    {
+        Invoke("Destroygemcanvas", 3f);
+    }
+    
+    public void Destroygemcanvas()
+    {
+        Destroy(GemCanvas);
+    }
+
+}
